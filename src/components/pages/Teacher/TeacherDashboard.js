bf7b59f8a9ba451e315c00583e3eb7489cf2ae05
Myeongwon André Jeon <maj@Myeongwons-MBP.fios-router.home>
2021-01-28 17:48:44 -0500

    Got the TeacherDashboard to look 80% like HeadmasterDashboard

--- src/components/pages/Teacher/TeacherDashboard.js
+++ src/components/pages/Teacher/TeacherDashboard.js
@@ -14,20 +14,83 @@ import Logout from '../../Logout.js';
 
 const TeacherDashboard = () => {
   const { userId } = useSelector(state => state.authReducer);
+  const [visible, setVisible] = useState(true);
+  const [desktop, setDesktop] = useState(true);
+
+  useEffect(() => {
+    if (window.innerWidth <= 800 || document.documentElement.width <= 800) {
+      setDesktop(false);
+      setVisible(false);
+    } else {
+      setDesktop(true);
+    }
+  }, []);
+
+  const onClose = () => {
+    setVisible(false);
+  };
+
+  // Todo: this needs to be converted to a mediaquery and removed from here
+  window.addEventListener('resize', () => {
+    if (window.innerWidth <= 800 || document.documentElement.width <= 800) {
+      setDesktop(false);
+      setVisible(false);
+    } else {
+      setDesktop(true);
+      setVisible(true);
+    }
+  });
 
   return (
-    <>
-      <h2>Hello, Teacher!!!</h2>
-      <div className="teacher__dashboard__navbar">
-        {/* subtracting userId from 10 just for temporary purpose */}
-        <NavLink to={`/teacher/${userId - 10}`}>
-          <button className="btn l2-btn menuLinks">Profile</button>
-        </NavLink>
-        <NavLink to="/logout">
-          <button className="btn l2-btn menuLinks">Logout</button>
-        </NavLink>
+    <div>
+      <Dashboard>
+        <Switch>
+          <Route path="/teacher" />
+          <Route path="/logout" component={Logout} />
+        </Switch>
+      </Dashboard>
+
+      {desktop ? null : (
+        // inline style to force animation
+        <div style={visible ? menuMove : menuIcon}>
+          <Button
+            type="primary"
+            style={menuButton} // inline style to override Ant Design
+            onClick={() => setVisible(!visible)}
+            icon={<MenuOutlined />}
+          >
+            Menu
+          </Button>
+        </div>
+      )}
+
+      <div>
+        <Drawer
+          placement={desktop ? 'left' : 'bottom'}
+          closable={false}
+          onClose={onClose}
+          visible={visible}
+          mask={false}
+          width={desktop ? 300 : 500}
+          height={500}
+        >
+          <h2>Hello, Teacher!!!</h2>
+
+          <div className="teacher__dashboard__navbar">
+            {/* subtracting userId from 10 just for temporary purpose */}
+            <NavLink
+              to={`/teacher/${userId - 10}`}
+              onClick={() => setVisible(true)}
+            >
+              <button className="btn l2-btn menuLinks">Profile</button>
+            </NavLink>
+            <Link to="/logout" onClick={() => setVisible(true)}>
+              <button className="btn l2-btn menuLinks">Logout</button>
+            </Link>
+          </div>
+        </Drawer>
       </div>
-    </>
+    </div>
   );
 };
 
